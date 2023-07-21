6f21796ae07ac6eca212ffb01fed9e841180d82c
Steve <steve.grassia@gmail.com>
2023-07-21 16:01:33 +0200

    started eng docs translation

--- src/ui/verbi/HelpVerboHandler.java
+++ src/ui/verbi/HelpVerboHandler.java
@@ -64,87 +64,87 @@ public class HelpVerboHandler extends VerboHandler {
 	}
 	
 	private void printClear() {
-		System.out.println("cls: pulisce tutte le definizioni di porte logiche e di segnali fatte precedentemente.\n");
+		System.out.println("cls: clears all previously placed circuit elements (gates and signals).\n");
 	}
 	
 	private void printDef() {
 		System.out.println(
-				"def: definisce un elemento nel circuito.\n"
-				+ "\tdef gate [nome_gate] [tipo_porta]: definisce una porta logica, dove:\n"
-				+ "\t\t-[nome_gate] è il nome della porta\n"
-				+ "\t\t-[tipo_porta] può essere and, or oppure low\n"
-				+ "\tdef signal [nome_segnale] [stato_iniziale]: definisce un segnale, dove:\n"
-				+ "\t\t-[nome_segnale] è il nome del segnale\n"
-				+ "\t\t-[stato_iniziale] è opzionale, e può essere high oppure low\n"
-				+ "\tdef event [tipo_evento] [nome_segnale] [istante]: definisce un evento nella timeline, dove:\n"
-				+ "\t\t-[tipo_evento]: può essere raise oppure fall\n"
-				+ "\t\t-[nome_segnale]: il segnale definito precedentemente a cui associare l'evento.\n"
-				+ "\t\t-[istante]: istante di tempo positivo in cui si deve verificare l'evento\n");
+				"def: defines a circuit element.\n"
+				+ "\tdef gate [gate_name] [gate_type]: defines a logic gate, where:\n"
+				+ "\t\t-[gate_name]:----------gate's name\n"
+				+ "\t\t-[gate_type]:----------can be: and, or, not\n\n"
+				+ "\tdef signal [signal_name] [initial_state]: defines a signal, where:\n"
+				+ "\t\t-[signal_name]:--------signal's name\n"
+				+ "\t\t-[initial_state]:------optional, can be either high or low\n\n"
+				+ "\tdef event [event_type] [signal_name] [time]: defines an event, where:\n"
+				+ "\t\t-[event_type]:---------can be either raise or fall\n"
+				+ "\t\t-[signal_name]:--------previously defined signal that the event must modify\n"
+				+ "\t\t-[time]:---------------time instant when the event must occur\n\n");
 	}
 	
 	private void printLink() {
 		System.out.println(
-				"link [nome_segnale] [nome_porta] [slot]: collega un segnale ad uno slot di una porta logica definita precedentemente, dove:\n"
-				+ "\t-[nome_segnale]: nome del segnale definito precedentemente, da collegare allo slot.\n"
-				+ "\t-[nome_porta]: nome della porta logica definita precedentemente, da collegare al segnale\n"
-				+ "\t-[slot]: slot della porta da collegare. Il formato è: "
-				+ "[I/O][slotIndex], dove:\n"
-				+ "\t\t-[I] indica che lo slot è di input.\n"
-				+ "\t\t-[O] indica che lo slot è di output.\n"
-				+ "\t\t-[slotIndex] è l'indice dello slot.\n"
-				+ "\t\tPer esempio, I0 indica il primo slot di ingresso, O1 indica il secondo slot di uscita.\n"
+				"link [signal_name] [gate_name] [slot]: links a signal to a slot of a previously defined gate, where:\n"
+				+ "\t-[signal_name]:------name of the previously defined signal to link to the slot.\n"
+				+ "\t-[gate_name]:--------name of the previously defined gate to be linked to the signal\n"
+				+ "\t-[slot]:-------------gate's slot to be linked. Its format is"
+				+ "[I/O][slotIndex], where:\n"
+				+ "\t\t-[I]---------means that the slot is an input slot.\n"
+				+ "\t\t-[O]---------means that the slot is an output slot.\n"
+				+ "\t\t-[slotIndex]-index of the slot.\n"
+				+ "\t\tFor example, I0 means first input slot, while O1 means second output slot.\n"
 				);
 	}
 	
 	private void printList() {
-		System.out.println("list: mostra la lista dei segnali e porte logiche definite fino ad ora.\n");
+		System.out.println("list: shows a list of all the signals and gates defined so far.\n");
 	}
 	
 	private void printPrint() {
-		System.out.println("print: chiedi al Sistema di visualizzare qualcosa.\n"
-				+ "\tprint [cosa_visualizzare] [parametri], dove:\n"
-				+ "\t\t[cosa_visualizzare] può essere:\n"
-				+ "\t\t\t-track. In questo caso [parametri] corrisponde al nome della traccia da visualizzare\n");
+		System.out.println("print: ask the program to visualize something:\n"
+				+ "\tprint [what_to_visualize] [parameters], where:\n"
+				+ "\t\t[what_to_visualize] can be:\n"
+				+ "\t\t\t-track. Prints a signal's track. Here, [parameters] must be the name of the track to print.\n");
 	}
 	private void printQuit() {
-		System.out.println("quit: esce dal programma.\n");
+		System.out.println("quit: exits the program.\n");
 	}
 	private void printSimulate() {
-		System.out.println("simulate: simula il circuito definito e convalidato.\n"
-				+ "\tsimulate [tempo] dove:\n"
-				+ "\t\t[tempo] è indica per quanto tempo si vuole simulare il circuito\n");
+		System.out.println("simulate: simulates the circuit.\n"
+				+ "\tsimulate [total_time] where:\n"
+				+ "\t\t[total_time]---For how much time the circuit must be simulated\n");
 	}
 	private void printUndef() {
 		System.out.println(
 				"undef: rimuove la definizione di un elemento dal circuito.\n"
 				+ "\tundef gate [nome_gate] [tipo_porta]: elimina una porta logica, dove:\n"
-				+ "\t\t-[nome_gate] è il nome della porta\n"
+				+ "\t\t-[nome_gate] ï¿½ il nome della porta\n"
 				+ "\tundef signal [nome_segnale]: definisce un segnale, dove:\n"
-				+ "\t\t-[nome_segnale] è il nome del segnale\n"
+				+ "\t\t-[nome_segnale] ï¿½ il nome del segnale\n"
 				+ "\tundef event [nome_segnale] [istante]: elimina un evento dalla timeline, dove:\n"
-				+ "\t\t-[nome_segnale]: il segnale a cui è associato l'evento.\n"
+				+ "\t\t-[nome_segnale]: il segnale a cui ï¿½ associato l'evento.\n"
 				+ "\t\t-[istante]: istante di tempo in cui si verifica l'evento\n");
 	}
 	private void printUnlink() {
 		System.out.println(
 				"unlink [nome_porta] [slot]: scollega un segnale da uno slot di una porta logica definita precedentemente, dove:\n"
 				+ "\t-[nome_porta]: nome della porta logica definita precedentemente in cui si trova lo slot da scollegare\n"
-				+ "\t-[slot]: slot della porta da scollegare. Il formato è: "
+				+ "\t-[slot]: slot della porta da scollegare. Il formato ï¿½: "
 				+ "[I/O][slotIndex], dove:\n"
-				+ "\t\t-[I] indica che lo slot è di input.\n"
-				+ "\t\t-[O] indica che lo slot è di output.\n"
-				+ "\t\t-[slotIndex] è l'indice dello slot.\n"
+				+ "\t\t-[I] indica che lo slot ï¿½ di input.\n"
+				+ "\t\t-[O] indica che lo slot ï¿½ di output.\n"
+				+ "\t\t-[slotIndex] ï¿½ l'indice dello slot.\n"
 				+ "\t\tPer esempio, I0 indica il primo slot di ingresso, O1 indica il secondo slot di uscita.\n"
 				);
 	}
 	
 	private void printValidate() {
-		System.out.println("validate: esegue la validazione del circuito.\n");
+		System.out.println("validate: executes circuit's validation.\n");
 	}
 	
 	private void printLoad() {
-		System.out.println("load: carica la definizione di circuito da file. NOTA: prima di caricare, elimina qualsiasi definizione fatta precedentemente.\n"
-				+ "\tload [nome_file] dove:\n"
-				+ "\t\t[nome_file] indica il nome del file che contiene la definizione del circuito.");
+		System.out.println("load: loads a circuit definition from a file. NB: before loading, this clears the circuit from all previously defined elements.\n"
+				+ "\tload [file_name] where:\n"
+				+ "\t\t[file_name]---name of the file to load");
 	}
 }
